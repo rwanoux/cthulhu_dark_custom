f73df4d742810d4a3e8d8a8b9255ff46d236a224
rwanoux <erwan_lemaire@yahoo.fr>
2025-03-04 08:51:33 +0100

    first init

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+var browserSync = require('browser-sync').create();
+
+function buildStyles() {
+    return gulp.src('./scss/rolenplay.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+
+
+};
+function reloadTemplates() {
+    return browserSync.reload("templates/**/*.html")
+}
+exports.buildStyles = buildStyles;
+exports.watch = function () {
+    browserSync.init(
+        {
+            server: false,
+            proxy: {
+                target: "https://localhost:30000/",
+                ws: true,
+            }
+
+        }
+    );
+    gulp.watch("./templates/**/*.html").on('change', reloadTemplates);
+
+    gulp.watch(['./scss/**/*.scss', './scss/*.scss'], buildStyles);
+
+};
